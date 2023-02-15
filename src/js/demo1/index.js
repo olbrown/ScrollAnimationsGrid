c1166e81143c4c453da28566384f79ba9aacf141
Oliver Brown <hi@olbrown.com>
2023-02-15 15:53:35 +0000

    Update 44 files

--- src/js/demo1/index.js
+++ src/js/demo1/index.js
@@ -1,46 +1,49 @@
-import { preloadImages } from '../utils';
-import Lenis from '@studio-freight/lenis'
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { preloadImages } from "../utils";
+import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-const gridItems = [...document.querySelectorAll('.grid > .grid__item')];
+const gridItems = [...document.querySelectorAll(".grid > .grid__item")];
 
 // Preload images
-preloadImages('.grid__item-img').then( _ => {
-	document.body.classList.remove('loading');
-	
-	// Smooth scrolling initialization (using Lenis https://github.com/studio-freight/lenis)
-	const lenis = new Lenis({
-		lerp: 0.1,
-		smooth: true,
-	});
-	const scrollFn = () => {
-		lenis.raf();
-		requestAnimationFrame(scrollFn);
-	};
-	requestAnimationFrame(scrollFn);
-	  
-	gridItems.forEach(item => {
-		
-		const image = item.querySelector('.grid__item-img');
+preloadImages(".grid__item-img").then((_) => {
+  document.body.classList.remove("loading");
 
-		gsap.timeline({
-			scrollTrigger: {
-                trigger: item,
-                start: "top top",
-                end: "bottom top",
-                scrub: true
-            }
-		})
-		.set(image, {
-			transformOrigin: `${gsap.utils.random(0,1) > 0.5 ? 0 : 100}% 100%`
-		})
-        .to(image, {
-            ease: 'none',
-            scale: 0
-        });
+  // Smooth scrolling initialization (using Lenis https://github.com/studio-freight/lenis)
+  const lenis = new Lenis({
+    lerp: 0.1,
+    smooth: true,
+  });
+  const scrollFn = () => {
+    lenis.raf();
+    requestAnimationFrame(scrollFn);
+  };
+  requestAnimationFrame(scrollFn);
 
-	});
+  gridItems.forEach((item) => {
+    const image = item.querySelector(".grid__item-img");
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: item,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .set(image, {
+        transformOrigin: `${gsap.utils.random(0, 1) > 0.5 ? 0 : 100}% 100%`,
+        filter: "brightness(100%) blur(0px)",
+        // transform: "translateY(0%)",
+      })
+      .to(image, {
+        ease: "none",
+        // opacity: 0,
+        filter: "brightness(400%) blur(40px)",
+        scale: 0.7,
+        // transform: "translateY(-20%)",
+      });
+  });
 });
-
